04f194d60919b7725dac1c4ef3f9ba4ee8c277e6
Md Sabbirul Latif Shuvo <sabbirul.latif@bjitgroup.com>
2023-09-25 11:01:23 +0600

    updated

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
+// import axios from 'axios'
 import { LOGIN_URL } from '@lib/api-urls'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const response = await axios.post(LOGIN_URL, req.body)
-  res.status(200).json(response.data)
+  // const response = await axios.post(LOGIN_URL, req.body)
+  // res.status(200).json(response.data)
+  res.status(200).json({ test: 'test', tesst: LOGIN_URL })
 }
